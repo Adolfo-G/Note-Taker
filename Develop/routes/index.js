0becd821864d28675fc054db76c2702ac5c7845e
Adolfo Gonzalez <agonzal177@gmail.com>
2022-02-14 17:48:39 -0500

    server files and routes

--- Develop/routes/index.js
+++ Develop/routes/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 
 //const tipsRouter = require('./tips');
+const notesRouter = require('./notes.js')
 
 const app = express();
 
 //app.use('/tips', tipsRouter);
+
+app.use('/notes', notesRouter)
 
 module.exports = app;
